c5fe7a35f403705ec9c20600f09df404b955d96a
hoshianaaa <hoshina54321@gmail.com>
2022-06-17 13:26:43 +0000

    add shape match start script

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ gnome-terminal --tab -t "roscore" -- bash -c "source /opt/ros/*/setup.bash; sour
 sleep 1
 gnome-terminal --tab -t "image publisher" -- bash -c "source /opt/ros/*/setup.bash; source $HOME/catkin_ws/devel/setup.bash; cd $PROGRAM_PATH; python img_pub.py search.png; exec bash"
 gnome-terminal --tab -t "image cripper" -- bash -c "source /opt/ros/*/setup.bash; source $HOME/catkin_ws/devel/setup.bash; cd $PROGRAM_PATH; python clipper_image_ros.py; exec bash"
+gnome-terminal --tab -t "shape_based_matching" -- bash -c "cd $PROGRAM_PATH/build; ./shape_based_matching_test; exec bash"
