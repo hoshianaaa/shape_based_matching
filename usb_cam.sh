cbfc2bb0ad0056ba5c32ff7dd2f4d1a5fe69e89b
hoshianaaa <hoshina54321@gmail.com>
2022-07-31 14:23:48 +0900

    update

--- usb_cam.sh
+++ usb_cam.sh
@@ -1,1 +1,4 @@
-rosrun usb_cam usb_cam_node _video_device:=/dev/video2
+# install
+# sudo apt install ros-noetic-usb-cam
+rosrun usb_cam usb_cam_node _video_device:=/dev/video0
+#rosrun usb_cam usb_cam_node _video_device:=/dev/video2
